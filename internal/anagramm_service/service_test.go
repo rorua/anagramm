e6f854d19c438ab278594ed868462234075d3ab9
Ravshan Yuldashev <38344113+rorua@users.noreply.github.com>
2021-08-25 13:18:05 +0500

    fixed tests

--- internal/anagramm_service/service_test.go
+++ internal/anagramm_service/service_test.go
@@ -2,7 +2,7 @@ package anagramm_service
 
 import "testing"
 
-func Test_find(t *testing.T) {
+func Test_findAll(t *testing.T) {
 
 	dict := []string{"foobar", "aabb", "baba", "boofar", "test"}
 
@@ -11,23 +11,23 @@ func Test_find(t *testing.T) {
 		input    string
 		expected []string
 	}{
-		{name: "test 1", input: "foobar", expected: []string{"foobar", "boofar"}},
-		{name: "test 2", input: "raboof", expected: []string{"foobar", "boofar"}},
-		{name: "test 3", input: "abba", expected: []string{"aabb", "baba"}},
-		{name: "test 4", input: "test", expected: []string{"test"}},
-		{name: "test 5", input: "qwerty", expected: []string{}},
+		{name: "test 1: foobar", input: "foobar", expected: []string{"foobar", "boofar"}},
+		{name: "test 2: raboof", input: "raboof", expected: []string{"foobar", "boofar"}},
+		{name: "test 3: abba", input: "abba", expected: []string{"aabb", "baba"}},
+		{name: "test 4: test", input: "test", expected: []string{"test"}},
+		{name: "test 5: qwerty", input: "qwerty", expected: []string{}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			output := findAll(dict, tt.input)
 			if len(output) != len(tt.expected) {
-				t.Fatalf("expected len %d, got %d", len(dict), len(tt.expected))
+				t.Fatalf("expected len %d, got %d", len(output), len(tt.expected))
 			}
 
 			for i, v := range tt.expected {
 				if v != output[i] {
-					t.Fatalf("expected len %d, got %d", len(dict), len(tt.expected))
+					t.Fatalf("expected %v, got %v", v, tt.expected)
 				}
 			}
 		})
